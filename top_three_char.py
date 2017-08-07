c799c5971021e9f0e7fa28bcb01403e521aff05e
patilpankaj5000 <30782425+patilpankaj5000@users.noreply.github.com>
2017-08-07 20:11:36 +0530

    Top Three common Characters
    Function which accepts a string 'S' as an input containing only lowercase English letters and returns top 3 most common characters in string as a list.
    
    For Example;
    Input: 'aaaaabbbccccdddef'
    Output: ['a','c','d']

--- top_three_char.py
+++ top_three_char.py
@@ -1,27 +1,32 @@
+# Function which accepts a string 'S' as an argument containing only lowercase English letters and 
+# returns top 3 most common characters in string as a list.
+
+# Creator: Pankaj Patil
+
+# Input : 'aaaaabbbccccdddef'
+# Output: ['a','c','b','d']
+# In above example, given string has top 3 most common characters as
+# a with frequency 5 
+# c with frequency 4
+# d with frequency 3
+
 def top_three_char(S):
     if S.islower():
-        letterdict={}
+        letterdict={}               
         for letter in S:
             letterdict[letter] = 0
         for letter in S:
             letterdict[letter] += 1
-        sorted_occurence=sorted(letterdict.values(),reverse=True)
-        top_three=[]
-        for i in range(len(sorted_occurence)):
-            if sorted_occurence[i] in top_three and len(top_three)<3:
-                continue
-            elif len(top_three)==3:
-                break
-            else:
-                top_three.append(sorted_occurence[i])
+        sorted_occurence=[]
+        tmp=[]
+        for i in letterdict:         
+            tmp.append([letterdict[i],i])
+        sorted_occurence=sorted(tmp,reverse=True)
+        #print(sorted_occurence)
         final=[]
-        for i in letterdict:
-            if letterdict[i] in top_three:                 
-                final.append(i)
-            elif len(final)==3:
-                break
-            else:
-                continue
+        for i in range(3):
+            final.append(sorted_occurence[i][1])
         return final
     else:
-        print("string should be lower case")
+        print("string should be in lower case")
+
